c990a44f6ba4f4a35c90d2e7876e8713c71f94de
刘普 <liupu@bettagames.com>
2023-09-21 18:17:43 +0800

    注释

--- src/main/java/com/github/pdfmerge/MergeCommand.java
+++ src/main/java/com/github/pdfmerge/MergeCommand.java
@@ -12,9 +12,15 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+/**
+ * 输入参数：--input=/location/f1.pdf --input=/location/f2.pdf
+ */
 @Command(mixinStandardHelpOptions = true)
 public class MergeCommand implements Callable<Integer> {
 
+    /**
+     * 根据参数设置，必须输入
+     */
     @CommandLine.Option(names = {"-i", "--input"}, required = true)
     private List<String> inputFileNames;
 
@@ -23,12 +29,14 @@ public class MergeCommand implements Callable<Integer> {
 
     @Override
     public Integer call() {
+        // 参数判断
         if (inputFileNames.size() < 2) {
             Log.error("At least two input files must be supplied");
             return 1;
         }
         Log.debugv("Will attempt to merge {0} into {1}", String.join(",", inputFileNames), outputFileName);
 
+        // 逻辑
         var utility = new PDFMergerUtility();
         var paths = inputFileNames.stream().map(Paths::get).toList();
         try {
